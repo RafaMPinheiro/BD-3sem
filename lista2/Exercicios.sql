df272bc2d54f46c975d10f81a07ff1fede52e124
RafaMPinheiro <Rafaelmessipinheiro@gmail.com>
2023-09-01 10:13:20 -0300

    lista 2 finalizada

--- lista2/Exercicios.sql
+++ lista2/Exercicios.sql
@@ -89,11 +89,24 @@ VALUES ('Ação Explosiva', 120), ('Comédia Louca', 90), ('Drama Intenso', 150)
 
 INSERT INTO
     public.sala (nome, capacidade)
-VALUES ('Sala A', 80), ('Sala B', 100), ('Sala C', 60), ('Sala D', 120), ('Sala E', 90);
+VALUES 
+('Sala A', 80),
+('Sala B', 100),
+('Sala C', 60),
+('Sala D', 120),
+('Sala E', 90);
 
 INSERT INTO
     externo.sessao (filme_id, sala_id, data, hora)
-VALUES (1, 1, '2023-08-28', '18:00:00'), (2, 2, '2023-08-28', '20:30:00'), (3, 3, '2023-08-29', '15:00:00'), (4, 4, '2023-08-29', '18:30:00'), (5, 5, '2023-08-30', '16:45:00');
+VALUES
+(1, 1, CURRENT_DATE, '18:00:00'),
+(2, 2, CURRENT_DATE, '20:30:00'),
+(3, 3, CURRENT_DATE, '15:00:00'),
+(4, 4, CURRENT_DATE, '18:30:00'),
+(5, 5, CURRENT_DATE, '16:45:00'),
+(2, 4, CURRENT_DATE, '18:30:00'),
+(2, 2, CURRENT_DATE, '18:30:00'),
+(4, 3, CURRENT_DATE, '18:30:00');
 
 INSERT INTO
     externo.ingresso (
@@ -104,26 +117,23 @@ INSERT INTO
         poltrona,
         valor_pago
     )
-VALUES (1, 1, 12.5, 'B', 10, 12.5), (2, 2, 15.0, 'C', 5, 15.0), (3, 3, 10.0, 'A', 8, 10.0), (4, 4, 13.0, 'D', 15, 13.0), (5, 5, 11.5, 'E', 20, 11.5);
+VALUES 
+(1, 1, 12.5, 'B', 10, 12.5),
+(2, 2, 15.0, 'C', 5, 15.0), 
+(3, 3, 10.0, 'A', 8, 10.0), 
+(4, 4, 13.0, 'D', 15, 13.0), 
+(5, 5, 11.5, 'E', 20, 11.5),
+(2, 6, 11.5, 'E', 20, 11.5),
+(1, 7, 11.5, 'E', 20, 11.5);
 
 INSERT INTO
     interno.setor (descricao, valor_por_hora)
-VALUES (
-        'Departamento de Vendas',
-        25.5
-    ), (
-        'Departamento de Recursos Humanos',
-        30.0
-    ), (
-        'Departamento de Tecnologia',
-        35.75
-    ), (
-        'Departamento Financeiro',
-        28.75
-    ), (
-        'Departamento de Marketing',
-        27.0
-    );
+VALUES 
+('Departamento de Vendas',25.5),
+('Departamento de Recursos Humanos',30.0),
+('Departamento de Tecnologia',35.75),
+('Departamento Financeiro',28.75),
+('Departamento de Marketing',27.0);
 
 INSERT INTO
     interno.funcionario (nome, setor_id)
@@ -136,33 +146,17 @@ INSERT INTO
         data_hora_entrada,
         data_hora_saida
     )
-VALUES (
-        1,
-        1,
-        '2023-08-28 09:00:00',
-        '2023-08-28 18:00:00'
-    ), (
-        2,
-        2,
-        '2023-08-28 10:30:00',
-        '2023-08-28 17:30:00'
-    ), (
-        3,
-        3,
-        '2023-08-28 08:45:00',
-        '2023-08-28 16:45:00'
-    ), (
-        4,
-        4,
-        '2023-08-29 09:30:00',
-        '2023-08-29 18:30:00'
-    ), (
-        5,
-        3,
-        '2023-08-29 10:00:00',
-        '2023-08-29 17:00:00'
-    );
-
+VALUES 
+(1, 1, '2023-08-28 08:00:00', '2023-08-28 12:00:00'),
+(2, 2, '2023-08-28 13:30:00', '2023-08-28 17:30:00'),
+(3, 3, '2023-08-28 08:00:00', '2023-08-28 12:00:00'),
+(4, 4, '2023-08-29 19:00:00', '2023-08-29 23:00:00'),
+(5, 3, '2023-08-29 08:00:00', '2023-08-29 12:00:00');
+(1,1,'2023-08-28 08:00:00','2023-08-28 16:00:00'),
+(2,2,'2023-08-28 14:00:00','2023-08-28 22:00:00'),
+(3,3,'2023-08-28 09:00:00','2023-08-28 16:45:00'),
+(4,4,'2023-08-29 09:30:00','2023-08-29 18:30:00'),
+(5,3,'2023-08-29 10:00:00','2023-08-29 17:00:00');
 INSERT INTO
     externo.ingresso (
         telespectador_id,
@@ -174,25 +168,72 @@ INSERT INTO
     )
 VALUES (3, 3, 12.5, 'B', 10, 12.5), (1, 2, 12.5, 'B', 10, 12.5);
 
+-- 2)
 SELECT
     telespectador.cpf,
     telespectador.nome,
-    sessao.id as id_sessao,
-    count(sessao.id) as qntd
+    STRING_AGG ( CAST(sessao.id AS text), ', ' ORDER BY sessao.id) "Sessão",
+    count(ingresso.id) as qntd
 FROM externo.ingresso
-    INNER JOIN externo.telespectador ON (
-        telespectador.id = ingresso.telespectador_id
-    )
+    INNER JOIN externo.telespectador ON (telespectador.id = ingresso.telespectador_id)
     INNER JOIN externo.sessao ON (ingresso.sessao_id = sessao.id)
-WHERE EXTRACT(
-        MONTH
-        FROM
-            sessao.data
-    ) = EXTRACT(
-        MONTH
-        FROM current_date
-    )
+WHERE (EXTRACT(MONTH FROM sessao.data) = EXTRACT(MONTH FROM current_date)) 
+AND (EXTRACT(YEAR FROM sessao.data) = EXTRACT(YEAR FROM current_date))
 GROUP BY
-    sessao.id,
     telespectador.cpf,
+    telespectador.nome
+ORDER BY
     telespectador.nome;
+
+-- 3)
+SELECT sala.id, CAST(AVG(ingresso.id) AS numeric (8,2)) FROM ingresso
+INNER JOIN sessao ON (sessao.id = ingresso.sessao_id)
+INNER JOIN sala ON (sala.id = sessao.sala_id)
+WHERE sessao.data >= current_date - INTERVAL '7 day'
+GROUP BY sala.id
+HAVING avg(ingresso.id) >= 5
+ORDER BY sala.id;
+
+-- 4)
+CREATE OR REPLACE VIEW ex4 AS SELECT telespectador.nome, telespectador.cpf
+FROM telespectador 
+INNER JOIN ingresso ON (telespectador.id = ingresso.telespectador_id)
+INNER JOIN sessao ON (ingresso.sessao_id = sessao.id)
+WHERE sessao.data = current_date
+ORDER BY RANDOM()
+LIMIT 1;
+SELECT * FROM ex4;
+
+-- 5)
+SELECT filme.id, filme.titulo, COUNT(ingresso.id) FROM sessao 
+LEFT JOIN filme ON (sessao.filme_id = filme.id)
+INNER JOIN ingresso on (ingresso.sessao_id = sessao.id)
+WHERE EXTRACT(YEAR FROM sessao.data) = EXTRACT(YEAR FROM CURRENT_DATE)
+GROUP BY filme.id
+HAVING COUNT(ingresso.id) = (SELECT COUNT(ingresso.id) FROM sessao 
+LEFT JOIN filme ON (sessao.filme_id = filme.id)
+INNER JOIN ingresso on (ingresso.sessao_id = sessao.id)
+WHERE EXTRACT(YEAR FROM sessao.data) = EXTRACT(YEAR FROM CURRENT_DATE)
+GROUP BY filme.id
+ORDER BY COUNT(ingresso.id) DESC LIMIT 1);
+
+-- 6)
+CREATE OR REPLACE VIEW ex6 AS SELECT sala.nome, filme.titulo, telespectador.nome, 
+TO_CHAR(sessao.data, 'DD/MM/YYYY'), TO_CHAR(sessao.hora, 'HH24:MI'),
+ingresso.corredor, ingresso.poltrona FROM telespectador
+INNER JOIN ingresso ON (telespectador.id = ingresso.telespectador_id)
+INNER JOIN sessao ON (ingresso.sessao_id = sessao.id)
+INNER JOIN filme ON (sessao.filme_id = filme.id)
+INNER JOIN sala ON (sessao.sala_id = sala.id)
+ORDER BY sala.nome;
+
+-- 7)
+SELECT 
+    CASE 
+        WHEN CAST(data_hora_entrada AS time) >= '08:00:00' AND CAST(data_hora_entrada AS time) <= '12:00:00' THEN 'Manhã'  
+        WHEN CAST(data_hora_entrada AS time) >= '13:30:00' AND CAST(data_hora_entrada AS time) <= '17:30:00' THEN 'Tarde'  
+        WHEN CAST(data_hora_entrada AS time) >= '19:00:00' AND CAST(data_hora_entrada AS time) <= '23:00:00' THEN 'Noite'  
+    END AS "Turno", COUNT(*) AS "Qtnd"
+FROM turno
+WHERE EXTRACT(YEAR FROM data_hora_entrada) = EXTRACT(YEAR FROM CURRENT_DATE)
+GROUP BY "Turno";
